22bc37ee24b76b00cc25204b39116fe8243319e0
aymericdelab <aymericdelabrousse@gmail.com>
2018-12-31 16:56:54 +0100

    ajoute tournament selection

---
+++ tsp/template/tournament.m
@@ -0,0 +1,18 @@
+function  NewChrIx = tournament(FitnV,Nsel);
+%UNTITLED3 Summary of this function goes here
+%   Detailed explanation goes here
+
+CurrPop=1;
+NewChrIx=zeros(Nsel,1);
+K=20;
+rand=zeros(K,1);
+
+while (CurrPop<Nsel+1)
+    rand=rand_int(K,1,[1 length(FitnV)]);
+    pool=[FitnV(rand),rand];
+    best=sortrows(pool,2);
+    NewChrIx(CurrPop)=best(1,2);
+    CurrPop=CurrPop+1;
+
+end
+
